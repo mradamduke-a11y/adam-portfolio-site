f121b62d5e5a407c4f0abfa8320341dcb477ac7d
Adam Duke <aduke0783@gmail.com>
2026-06-30 18:30:36 -0400

    Update site

--- Push Index.bat
+++ Push Index.bat
@@ -1,7 +1,7 @@
 @echo off
 echo === Pushing index.html to adamdukeugc.com ===
 echo.
-curl -T "C:\Users\mrada\Claude\Projects\Adam - Portfolio Site\www\index.html" "ftp://claude:ChesterCheese1!@adamdukeugc.com/adamdukeugc/index.html" --ftp-pasv --progress-bar
+curl -T "C:\Users\mrada\Claude\Projects\Adam - Portfolio Site\www\index.html" "ftp://dukesbread:P00lish1@adamdukeugc.com/adamdukeugc/index.html" --ftp-pasv --progress-bar
 echo.
 if %ERRORLEVEL% == 0 (
   echo SUCCESS - index.html is live.
